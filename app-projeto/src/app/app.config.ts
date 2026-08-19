a6505b09a6319947b71f79f57c793b9286833aef
Matheus <matheus.gabriel.les@gmail.com>
2026-08-19 20:27:40 -0300

    implementacao app

--- app-projeto/src/app/app.config.ts
+++ app-projeto/src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 import Aura from '@primeuix/themes/aura';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
         preset: Aura
--- app-projeto/src/app/app.ts
+++ app-projeto/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
+  standalone: true,
+  providers: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('app-projeto');
+  
 }
