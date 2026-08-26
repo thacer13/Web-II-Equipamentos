8479eae159d5c935c141b75580455c4e1d53b49c
Fabr1c1oRP <fabriciorplautz@gmail.com>
2026-08-26 10:56:56 -0300

    Adiciona cliente.component e implementa HTML + CSS

--- app-projeto/src/app/app.routes.ts
+++ app-projeto/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import{LoginComponent} from './auth/login/login';
 import { HomeComponent } from './inicial/home';
 import { CadastroComponent } from './auth/cadastro/cadastro';
+import { ClienteComponent } from './cliente/cliente.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,11 @@ export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    
+    },
+    {
+        path: 'cliente',
+        component: ClienteComponent
     
     }
 ]
