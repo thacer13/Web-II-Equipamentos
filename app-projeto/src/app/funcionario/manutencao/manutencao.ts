a325dc6f00ddc47eb95117f1af4fa555ad830344
Procha976 <paulorobertororochaw@gmail.com>
2026-09-09 23:37:24 -0300

    Tela de manutenção

---
+++ app-projeto/src/app/funcionario/manutencao/manutencao.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-manutencao',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule
+  ],
+  templateUrl: './manutencao.html',
+})
+export class ManutencaoComponent {
+  // Dados da solicitação (viriam do backend)
+  cliente: string = 'Nome do Cliente';
+  produto: string = 'Notebook Dell Inspiron 15';
+  descricaoProblema: string = 'Tela quebrada';
+
+  funcionarioLogado: string = 'Funcionário Exemplo';
+
+  descricaoManutencao: string = '';
+  orientacoesCliente: string = '';
+
+  onConfirmarManutencao() {
+    console.log('Descrição:', this.descricaoManutencao);
+    console.log('Orientações:', this.orientacoesCliente);
+    console.log('Funcionário:', this.funcionarioLogado);
+    console.log('Data/Hora:', new Date());
+    console.log('Status: ARRUMADA');
+  }
+
+  onRedirecionarClick() {
+    // RF015 - Redirecionar Manutenção
+    console.log('Redirecionar manutenção');
+  }
+}
