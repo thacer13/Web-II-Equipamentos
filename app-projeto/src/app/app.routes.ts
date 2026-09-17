9628ae9728d95ffcf2aa19276cd5a2cbea7ab113
Fabr1c1oRP <fabriciorplautz@gmail.com>
2026-09-16 23:28:45 -0300

    Termina telas entrega parcial

--- app-projeto/src/app/app.routes.ts
+++ app-projeto/src/app/app.routes.ts
@@ -3,9 +3,15 @@ import{LoginComponent} from './auth/login/login';
 import { CadastroComponent } from './auth/cadastro/cadastro';
 import { ErrComponent } from './auth/err/err';
 import { ClienteComponent } from './cliente/cliente.component';
+import { FuncionarioComponent } from './funcionario/solicitacoes/solicitacao-funcionario';
+import { EfetuarOrcamentoComponent } from './efetuar-orcamento/efetuar-orcamento.component';
+import { CategoriasComponent } from './funcionario/categorias/categorias';
 import { FuncionariosComponent } from './funcionario/lista-funcionarios/funcionarios';
-import { ManutencaoComponent } from './funcionario/manutencao/manutencao';
+import { LoginFuncionarioComponent } from './funcionario/login-funcionario/login-funcionario';
+import { RelatorioPeriodoComponent } from './funcionario/relatorio-periodo/relatorio-periodo';
+import { RelatorioCategoriasComponent } from './funcionario/relatorio-categorias/relatorio-categorias';
 import { RedirecionamentoComponent } from './funcionario/manutencao/redirecionamento';
+import { ManutencaoComponent } from './funcionario/manutencao/manutencao';
 
 export const routes: Routes = [
     {
@@ -15,7 +21,6 @@ export const routes: Routes = [
     {
         path: 'auth/cadastro',
         component: CadastroComponent
-    
     },
     {
         path: 'login',
@@ -25,7 +30,6 @@ export const routes: Routes = [
         path: '',
         redirectTo: 'auth/login',
         pathMatch: 'full'
-    
     },
     {
         path: 'auth/err',
@@ -37,7 +41,31 @@ export const routes: Routes = [
     },
     {
         path: 'funcionario',
+        component: FuncionarioComponent
+    },
+    {
+        path: 'funcionario/categorias',
+        component: CategoriasComponent
+    },
+    {
+        path: 'funcionario/lista-funcionarios',
         component: FuncionariosComponent
+    },
+    {
+        path: 'funcionario/login-funcionario',
+        component: LoginFuncionarioComponent
+    },
+    {
+        path: 'funcionario/receitas/periodo',
+        component: RelatorioPeriodoComponent
+    },
+    {
+        path: 'funcionario/receitas/categoria',
+        component: RelatorioCategoriasComponent
+    },
+    {
+        path: 'efetuar-orcamento/:id',
+        component: EfetuarOrcamentoComponent
     },
     {
         path: 'manutencao',
